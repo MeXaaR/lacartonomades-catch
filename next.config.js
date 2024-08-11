27e61e8b1f3ecaf41f8b7e1d043444a77f14e039
Francois AUBEUT <francois.aubeut-gervaise@carrier.com>
2024-08-11 11:08:03 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-// next.config.js
 module.exports = {
-    experimental: {
-        headers() {
-            return [
-                {
-                    source: "/.well-known/apple-app-site-association",
-                    headers: [{ key: "content-type", value: "application/json" }]
-                }
-            ];
-        }
-    }
+    async headers() {
+        return [
+            {
+                source: '/.well-known/apple-app-site-association',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json',
+                    },
+                ],
+            },
+        ];
+    },
 };
